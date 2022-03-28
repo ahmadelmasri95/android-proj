86087f3c843323993b1aa8c0144253286f1d0563
Ahmad ElMasri <ahmad.elmasri@ucalgary.ca>
2022-03-28 12:48:35 -0600

    Updated UI

--- app/src/main/java/com/example/personaltracker/MainActivity.java
+++ app/src/main/java/com/example/personaltracker/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.personaltracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar snackbar = Snackbar.make(v, "Hello there", Snackbar.LENGTH_SHORT);
+                snackbar.show();
+            }
+        });
     }
+
 }
