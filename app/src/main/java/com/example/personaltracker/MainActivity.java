c35d2c9ec606fa6e5950c49b7fd909f90142806b
Ahmad ElMasri <ahmad.elmasri@ucalgary.ca>
2022-04-02 21:26:23 -0600

    Refactor mainActivity

--- app/src/main/java/com/example/personaltracker/MainActivity.java
+++ app/src/main/java/com/example/personaltracker/MainActivity.java
@@ -2,14 +2,24 @@ package com.example.personaltracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AppOpsManager;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 //This activity retrieves the app usage from instagram and whatsapp
 
@@ -18,24 +28,25 @@ public class MainActivity extends AppCompatActivity {
     private FloatingActionButton floatingActionButton;
     private TextView textView1;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //initialize views
         floatingActionButton = this.findViewById(R.id.floatingActionButton);
         textView1 = this.findViewById(R.id.textView1);
-        Toast.makeText(this, "activity started", Toast.LENGTH_SHORT).show();
 
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Retrieve the app usage time
-               startService(new Intent(getApplication(),AppUsageService.class));
             }
         });
     }
-    public void getAppUsage(){
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
-
 }
