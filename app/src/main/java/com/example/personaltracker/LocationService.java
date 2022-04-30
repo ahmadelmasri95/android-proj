c7acf74b9cb22c0a5fecee2eaa483ada8341180f
Ahmad ElMasri <ahmad.elmasri@ucalgary.ca>
2022-04-29 23:57:56 -0600

    Tested continuous location results and added broadcast receiver

--- app/src/main/java/com/example/personaltracker/LocationService.java
+++ app/src/main/java/com/example/personaltracker/LocationService.java
@@ -1,18 +1,37 @@
 package com.example.personaltracker;
 
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.IBinder;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
 
 
+import androidx.annotation.ColorInt;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 
 public class LocationService extends Service {
 
+    public static final String TRANSFER_DATA_ACTION = "TRANSFER_DATA_TO_UI";
     private LocationCallback locationCallback = new LocationCallback() {
         @Override
         public void onLocationResult(@NonNull LocationResult locationResult) {
@@ -20,6 +39,15 @@ public class LocationService extends Service {
             if (locationResult != null && locationResult.getLastLocation() != null) {
                 double latitude = locationResult.getLastLocation().getLatitude();
                 double longitude = locationResult.getLastLocation().getLongitude();
+                Log.d("LOCATION_UPDATE", latitude + " " + longitude);
+//                Toast.makeText(getApplicationContext(),"LOCATION_UPDATE" + latitude + " " + longitude,
+//                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent();
+                intent.setAction(TRANSFER_DATA_ACTION);
+                intent.putExtra("lati", latitude);
+                intent.putExtra("longi", longitude);
+                sendBroadcast(intent);
+
             }
         }
     };
@@ -28,5 +56,70 @@ public class LocationService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         throw new UnsupportedOperationException("Not yet Implemented");
+    }
+
+    public void startLocationService() {
+        String channelId = "location_notification_channel";
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent resultIntent = new Intent();
+        PendingIntent pendingIntent = PendingIntent.getActivity(this.getApplicationContext(),
+                0, resultIntent, PendingIntent.FLAG_IMMUTABLE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(
+                this.getApplicationContext(), channelId
+        );
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentTitle("Location Service");
+        builder.setDefaults(NotificationCompat.DEFAULT_ALL);
+        builder.setContentText("Running");
+        builder.setContentIntent(pendingIntent);
+        builder.setAutoCancel(false);
+        builder.setPriority(NotificationCompat.PRIORITY_MAX);
+        builder.setColor(getResources().getColor(R.color.green));
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (notificationManager != null
+                    && notificationManager.getNotificationChannel(channelId) == null) {
+                NotificationChannel notificationChannel = new NotificationChannel(channelId,
+                        "Location Service",
+                        NotificationManager.IMPORTANCE_HIGH);
+                notificationChannel.setDescription("This channel is used by Location Service");
+                notificationManager.createNotificationChannel(notificationChannel);
+            }
+        }
+
+        LocationRequest locationRequest = LocationRequest.create()
+                .setInterval(4000)
+                .setFastestInterval(2000)
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED
+        ) {
+            LocationServices.getFusedLocationProviderClient(this)
+                    .requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+        }
+        startForeground(Constants.LOCATION_SERVICE_ID, builder.build());
+    }
+
+    private void stopLocationService() {
+        LocationServices.getFusedLocationProviderClient(this).removeLocationUpdates(locationCallback);
+        stopForeground(true);
+        stopSelf();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent != null) {
+            String action = intent.getAction();
+            if (action != null) {
+                if (action.equals(Constants.ACTION_START_LOCATION_SERVICE)) {
+                    startLocationService();
+                } else if (action.equals(Constants.ACTION_STOP_LOCATION_SERVICE)) {
+                    stopLocationService();
+                }
+            }
+        }
+        //can return START_STICKY if i want background service to run forever until stopped
+        return super.onStartCommand(intent, flags, startId);
     }
 }
--- app/src/main/java/com/example/personaltracker/MainActivity.java
+++ app/src/main/java/com/example/personaltracker/MainActivity.java
@@ -5,8 +5,17 @@ import static retrofit2.Response.error;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 
+import android.Manifest;
+import android.app.ActivityManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ImageView;
@@ -14,6 +23,7 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.personaltracker.api.ApiUtilities;
+import com.google.android.material.textview.MaterialTextView;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -30,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
     private ProgressBar progressBar;
     private Handler alertHandler;
 
-
     //for the animated button
     private AppCompatButton button;
     private Handler animationHandler;
@@ -38,85 +47,10 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imageAnimation;
     private ImageView imageAnimation2;
     private ImageView imageView;
+    private MaterialTextView textView;
+    private static final int REQUEST_CODE_LOCATION_PERMISSION = 1;
 
     //check if can use one handler for all the runnable
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //initialize MainActivity views
-        init();
-        //Create searchButton pulse animations and change state of selector
-        button.setOnClickListener(v -> {
-            if (AnimationStatus) {
-                stopPulse();
-                button.setText(R.string.button_initial_state);
-            } else {
-                startPulse();
-                button.setText(R.string.button_secondary_state);
-            }
-            AnimationStatus = !AnimationStatus;
-        });
-    }
-
-    public void init() {
-        progressBar = this.findViewById(R.id.progressBar);
-        button = this.findViewById(R.id.searchButton);
-        imageAnimation = this.findViewById(R.id.img_animation);
-        imageAnimation2 = this.findViewById(R.id.img_animation2);
-        animationHandler = new Handler(this.getMainLooper());
-        alertHandler = new Handler(this.getMainLooper());
-        imageView = this.findViewById(R.id.bored1);
-    }
-
-    public void startPulse() {
-        animationHandler.post(animationRunnable);
-        alertHandler.postDelayed(progressRunnable, 8000);
-        //  getData();
-
-//        RequestOptions options = new RequestOptions()
-//                .centerCrop()
-//                .placeholder(R.mipmap.ic_launcher_round)
-//                .error(R.mipmap.ic_launcher_round);
-//        Glide.with(this)
-//                .load("https://image.thum.io/get/auth/57066-Masri/png/" + textInputEditText.getText().toString())
-//                .into(imageView);
-
-
-        //to change color of progressbar
-//        progressBar.getIndeterminateDrawable().setColorFilter(
-//                getResources().getColor(R.color.green),
-//                android.graphics.PorterDuff.Mode.SRC_IN);
-    }
-
-    public void stopPulse() {
-        animationHandler.removeCallbacks(animationRunnable);
-        alertHandler.removeCallbacks(progressRunnable);
-        progressBar.setIndeterminate(false);
-
-    }
-
-    public void getData() {
-        //Asynchronous rest api call - GET
-        ApiUtilities.getApiInterface().getScreenshot("https://www.nba.com")
-                .enqueue(new Callback<ResponseBody>() {
-                    @Override
-                    public void onResponse(@NonNull Call<ResponseBody> call, @NonNull Response<ResponseBody> response) {
-                        if (response.isSuccessful()) {
-                            if (response.body() != null) {
-                            }
-
-                        } else {
-                            Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(@NonNull Call<ResponseBody> call, @NonNull Throwable t) {
-                        Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-    }
 
     private final Runnable animationRunnable = new Runnable() {
         //Animates the main search button
@@ -144,6 +78,175 @@ public class MainActivity extends AppCompatActivity {
             progressBar.setIndeterminate(true);
         }
     };
+
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            double latitude = intent.getDoubleExtra("lati", 0);
+            double longitude = intent.getDoubleExtra("longi", 0);
+            textView.setText(latitude + " " + longitude);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //initialize MainActivity views
+        init();
+        //Create searchButton pulse animations and change state of selector
+        button.setOnClickListener(v -> {
+            if (AnimationStatus) {
+                stopLocationService();
+                stopPulse();
+                button.setText(R.string.button_initial_state);
+            } else {
+                if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            REQUEST_CODE_LOCATION_PERMISSION);
+                } else {
+                    startLocationService();
+                }
+                startPulse();
+                button.setText(R.string.button_secondary_state);
+            }
+            AnimationStatus = !AnimationStatus;
+        });
+    }
+
+    @Override
+    protected void onStart() { //can use this to start getting the location instead of a button press
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(LocationService.TRANSFER_DATA_ACTION);
+        registerReceiver(broadcastReceiver, intentFilter);
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        unregisterReceiver(broadcastReceiver);
+        super.onStop();
+    }
+
+    private void init() {
+        progressBar = this.findViewById(R.id.progressBar);
+        button = this.findViewById(R.id.searchButton);
+        imageAnimation = this.findViewById(R.id.img_animation);
+        imageAnimation2 = this.findViewById(R.id.img_animation2);
+        animationHandler = new Handler(this.getMainLooper());
+        alertHandler = new Handler(this.getMainLooper());
+        imageView = this.findViewById(R.id.bored1);
+        textView = this.findViewById(R.id.coordinates);
+    }
+
+    private void startPulse() {
+        animationHandler.post(animationRunnable);
+        alertHandler.postDelayed(progressRunnable, 8000);
+
+        //  getData();
+
+//        RequaestOptions options = new RequestOptions()
+//                .centerCrop()
+//                .placeholder(R.mipmap.ic_launcher_round)
+//                .error(R.mipmap.ic_launcher_round);
+//        Glide.with(this)
+//                .load("https://image.thum.io/get/auth/57066-Masri/png/" + textInputEditText.getText().toString())
+//                .into(imageView);
+
+
+        //to change color of progressbar
+//        progressBar.getIndeterminateDrawable().setColorFilter(
+//                getResources().getColor(R.color.green),
+//                android.graphics.PorterDuff.Mode.SRC_IN);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_LOCATION_PERMISSION && grantResults.length > 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationService();
+            } else {
+                Toast.makeText(this, "Permission denied!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void stopPulse() {
+        animationHandler.removeCallbacks(animationRunnable);
+        alertHandler.removeCallbacks(progressRunnable);
+        progressBar.setIndeterminate(false);
+    }
+
+    private void startLocationService() {
+        if (!isLocationServiceRunning()) {
+            Intent intent = new Intent(getApplicationContext(), LocationService.class);
+            intent.setAction(Constants.ACTION_START_LOCATION_SERVICE);
+            startService(intent);
+            Toast.makeText(this, "Location service initiated.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void stopLocationService() {
+        if (isLocationServiceRunning()) {
+            Intent intent = new Intent(getApplicationContext(), LocationService.class);
+            intent.setAction(Constants.ACTION_STOP_LOCATION_SERVICE);
+            startService(intent);
+            Toast.makeText(this, "Location service stopped.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void getData() {
+        //Asynchronous rest api call - GET
+        ApiUtilities.getApiInterface().getScreenshot("https://www.nba.com")
+                .enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(@NonNull Call<ResponseBody> call, @NonNull Response<ResponseBody> response) {
+                        if (response.isSuccessful()) {
+                            if (response.body() != null) {
+                            }
+
+                        } else {
+                            Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull Call<ResponseBody> call, @NonNull Throwable t) {
+                        Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private boolean isLocationServiceRunning() {
+        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        if (activityManager != null) {
+            for (ActivityManager.RunningServiceInfo service :
+                    activityManager.getRunningServices(Integer.MAX_VALUE)) {
+                if (LocationService.class.getName().equals(service.service.getClassName())) {
+                    if (service.foreground) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        return false;
+    }
 
     @Override
     protected void onDestroy() {
